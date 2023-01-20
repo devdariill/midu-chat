e2c60e0d0754d941c77cfc678d3b542f97c46855
nATOr <gowluonade@gmail.com>
2023-01-20 02:19:35 -0500

    TODO: create policy table 'messages'  select all anon

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,18 @@
+import { supabase } from "~/utils/supabase";
+import { useLoaderData } from "@remix-run/react";
+export const loader = async ({}) => {
+  const { data } = await supabase.from("messages").select();
+  console.log(data)
+  return { data };
+};
 export default function Index() {
+  const { data } = useLoaderData();
   return (
-    <h1>midudev</h1>
-  );
+    <main>
+      <h1>midudev</h1>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </main>
+  )
 }
